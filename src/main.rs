bd99f25883892e6870a46cbedaa2beef049cdc9f
HeisenbergHK <92860076+HeisenbergHK@users.noreply.github.com>
2024-04-19 16:38:10 +0330

    Adding the random number generator and compairing the two number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is :{secret_number}");
+
     println!("Enter your number:");
 
     let mut guess = String::new();
@@ -10,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read the line!");
 
+    let guess: u32 = guess.trim().parse().expect("Please enter an integer.");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Go higher."),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Go lower."),   
+    }
 }
