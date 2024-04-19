1d6b45f3a73f9d4c0cc645ce9cd91f96e8f7b399
HeisenbergHK <92860076+HeisenbergHK@users.noreply.github.com>
2024-04-19 16:45:38 +0330

    Making a lopp for the user until viktory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
-use std::io;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -8,21 +8,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is :{secret_number}");
 
-    println!("Enter your number:");
+    loop {
+        println!("Enter your number:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read the line!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read the line!");
+        let guess: u32 = guess.trim().parse().expect("Please enter an integer.");
 
-    let guess: u32 = guess.trim().parse().expect("Please enter an integer.");
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Go higher."),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Go lower."),   
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Go higher."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Go lower."),
+        }
     }
 }
